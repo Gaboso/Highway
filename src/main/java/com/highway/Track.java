58d5278a1b1b90f36ad9cad9fd543e1ccf53e014
Gaboso <gabrielsantiago2@gmail.com>
2016-08-14 10:51:07 -0400

    Added Log4j; Added annotation @override in some methods; Removed useless variables; Game screenshot was updated.

--- src/main/java/com/highway/Track.java
+++ src/main/java/com/highway/Track.java
@@ -24,35 +24,37 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+import org.apache.log4j.Logger;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-import static java.awt.Color.gray;
-import static java.awt.Color.yellow;
-
+/**
+ * Track Class
+ */
 public class Track extends JFrame implements Runnable {
-    int x, y;
-    int xDirection, yDirection, rectx, recty;
-    int bx, by;
 
-    Font font = new Font("ARIAL", Font.BOLD | Font.ITALIC, 20);
-    int t1 = 0, score = 0;
+    private static final Logger LOGGER = Logger.getLogger(Track.class);
 
-    double pp = 0.0;
-    double pp1 = 0.0;
+    //Colors
+    private static final Color BACKGROUND_COLOR = Color.decode("#4CAF50");
+    private static final Color OTHER_CAR_ONE = Color.decode("#D50000");
+    private static final Color OTHER_CAR_TWO = Color.decode("#00C853");
+    private static final Color ME_CAR = Color.decode("#0091EA");
 
-    Rectangle MyShip = new Rectangle(x, y, 20, 50);
-    int controlx = 1, controly = 1;
+    int x;
+    int y;
+    int xDirection;
+    int yDirection;
+
+    Rectangle myShip = new Rectangle(x, y, 20, 50);
     int yy = 1;
 
-    Rectangle boundary = new Rectangle(5, 380, 500, 20);
     Rectangle road = new Rectangle(65, 0, 285, 1000);
     Rectangle boundary2 = new Rectangle(45, 20, 20, 500);
     Rectangle boundary3 = new Rectangle(350, 20, 20, 500);
-
-    int c = 1;
 
     Rectangle c1 = new Rectangle(100, 380, 20, 30);
     Rectangle c2 = new Rectangle(300, 350, 30, 10);
@@ -63,14 +65,28 @@ public class Track extends JFrame implements Runnable {
     Rectangle illu13 = new Rectangle(45, 300, 20, 20);
     Rectangle illu23 = new Rectangle(350, 300, 20, 20);
 
-    int sd = 0, s1 = -30, s2 = 0;
+    int sd = 0;
+    int s1 = -30;
+    int s2 = 0;
     int w = 0;
 
-    private Graphics dbg;
-    private Image dbImage;
+    public Track() {
+        setBackground(BACKGROUND_COLOR);
+        addKeyListener(new AL());
+        setTitle("Speed Up");
+        setSize(500, 400);
+        setResizable(false);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        myShip.x = 200;
+        myShip.y = 350;
+        yy = 1;
+    }
 
     public void move() {
-        int cv = 2, cd = 2;
+        int cv = 2;
+        int cd = 2;
 
         if (w % 13 == 0)
             cv = 5;
@@ -84,8 +100,8 @@ public class Track extends JFrame implements Runnable {
         else if (w % 100 == 0)
             cd = 15;
 
-        MyShip.x += xDirection;
-        MyShip.y += yDirection;
+        myShip.x += xDirection;
+        myShip.y += yDirection;
 
         c1.y += cd;
         c2.y += cv;
@@ -102,14 +118,14 @@ public class Track extends JFrame implements Runnable {
             illu22.y -= yDirection;
             illu23.y -= yDirection;
         }
-        if (MyShip.x <= 50)
-            MyShip.x = 50;
-        if (MyShip.x >= 350)
-            MyShip.x = 350;
-        if (MyShip.y <= 150)
-            MyShip.y = 150;
-        if (MyShip.y > 380)
-            MyShip.y = 380;
+        if (myShip.x <= 50)
+            myShip.x = 50;
+        if (myShip.x >= 350)
+            myShip.x = 350;
+        if (myShip.y <= 150)
+            myShip.y = 150;
+        if (myShip.y > 380)
+            myShip.y = 380;
 
         if (c1.x <= 50)
             c1.x = 50;
@@ -143,6 +159,7 @@ public class Track extends JFrame implements Runnable {
             illu23.y = 50;
     }
 
+    @Override
     public void run() {
         try {
             while (true) {
@@ -150,19 +167,21 @@ public class Track extends JFrame implements Runnable {
                 Thread.sleep(20);
             }
         } catch (Exception e) {
-            System.err.println(e.getMessage());
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
-    public void setXDirection(int xdir) {
-        xDirection = xdir;
-    }
-
-    public void setYDirection(int ydir) {
-        yDirection = ydir;
-    }
-
     public class AL extends KeyAdapter {
+
+        private void setXDirection(int xdir) {
+            xDirection = xdir;
+        }
+
+        private void setYDirection(int ydir) {
+            yDirection = ydir;
+        }
+
+        @Override
         public void keyPressed(KeyEvent e) {
             int keyCode = e.getKeyCode();
             if (keyCode == KeyEvent.VK_LEFT)
@@ -175,6 +194,7 @@ public class Track extends JFrame implements Runnable {
                 setYDirection(+3);
         }
 
+        @Override
         public void keyReleased(KeyEvent e) {
             int keyCode = e.getKeyCode();
             if (keyCode == KeyEvent.VK_LEFT)
@@ -188,34 +208,23 @@ public class Track extends JFrame implements Runnable {
         }
     }
 
-    public Track() {
-        setBackground(Color.GREEN);
-        addKeyListener(new AL());
-        setTitle("SPEED UP");
-        setSize(500, 400);
-        setResizable(false);
-        setVisible(true);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        MyShip.x = 200;
-        MyShip.y = 350;
-        yy = 1;
-    }
-
+    @Override
     public void paint(Graphics g) {
-        dbImage = createImage(getWidth(), getHeight());
-        dbg = dbImage.getGraphics();
+        Image image = createImage(getWidth(), getHeight());
+        Graphics dbg = image.getGraphics();
         paintComponent(dbg);
-        g.drawImage(dbImage, 0, 0, this);
+        g.drawImage(image, 0, 0, this);
     }
 
-    public void paintComponent(Graphics g) {
-        g.setColor(yellow);
+    private void paintComponent(Graphics g) {
+        g.setColor(Color.YELLOW);
         g.fillRect(boundary2.x, boundary2.y, boundary2.width, boundary2.height);
         g.fillRect(boundary3.x, boundary3.y, boundary3.width, boundary3.height);
-        g.setColor(gray);
+
+        g.setColor(Color.GRAY);
         g.fillRect(road.x, road.y, road.width, road.height);
-        g.setColor(Color.white);
+
+        g.setColor(Color.WHITE);
         g.fillRect(illu11.x, illu11.y, illu11.width, illu11.height);
         g.fillRect(illu11.x + 170, illu11.y, 5, 25);
         g.fillRect(illu12.x, illu12.y, illu12.width, illu12.height);
@@ -225,35 +234,44 @@ public class Track extends JFrame implements Runnable {
         g.fillRect(illu21.x, illu21.y, illu21.width, illu21.height);
         g.fillRect(illu22.x, illu22.y, illu22.width, illu22.height);
         g.fillRect(illu23.x, illu23.y, illu23.width, illu23.height);
-        g.setColor(Color.red);
-        g.fillRect(MyShip.x, MyShip.y, 20, 50);
-        g.setColor(Color.black);
-        g.fillRect(MyShip.x + 20, MyShip.y + 5, 5, 5);
-        g.fillRect(MyShip.x - 5, MyShip.y + 5, 5, 5);
-        g.fillRect(MyShip.x + 20, MyShip.y + 40, 5, 5);
-        g.fillRect(MyShip.x - 5, MyShip.y + 40, 5, 5);
-        g.setColor(Color.yellow);
-        g.fillRect(c1.x, MyShip.y - s1 - sd, 20, 50);
-        g.setColor(Color.black);
-        g.fillRect(c1.x + 20, MyShip.y - s1 - sd + 5, 5, 5);
-        g.fillRect(c1.x - 5, MyShip.y - s1 - sd + 5, 5, 5);
-        g.fillRect(c1.x + 20, MyShip.y - s1 - sd + 40, 5, 5);
-        g.fillRect(c1.x - 5, MyShip.y - s1 - sd + 40, 5, 5);
-        g.setColor(Color.orange);
-        g.fillRect(c2.x, MyShip.y - s2 - sd, 20, 50);
-        g.setColor(Color.black);
-        g.fillRect(c2.x + 20, MyShip.y - s2 - sd + 5, 5, 5);
-        g.fillRect(c2.x - 5, MyShip.y - s2 - sd + 5, 5, 5);
-        g.fillRect(c2.x + 20, MyShip.y - s2 - sd + 40, 5, 5);
-        g.fillRect(c2.x - 5, MyShip.y - s2 - sd + 40, 5, 5);
-        g.drawString("Car - Dist. travelled", 380, 170);
 
+        g.setColor(ME_CAR);
+        g.fillRect(myShip.x, myShip.y, 20, 50);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(myShip.x + 20, myShip.y + 5, 5, 5);
+        g.fillRect(myShip.x - 5, myShip.y + 5, 5, 5);
+        g.fillRect(myShip.x + 20, myShip.y + 40, 5, 5);
+        g.fillRect(myShip.x - 5, myShip.y + 40, 5, 5);
+
+        g.setColor(OTHER_CAR_ONE);
+        g.fillRect(c1.x, myShip.y - s1 - sd, 20, 50);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(c1.x + 20, myShip.y - s1 - sd + 5, 5, 5);
+        g.fillRect(c1.x - 5, myShip.y - s1 - sd + 5, 5, 5);
+        g.fillRect(c1.x + 20, myShip.y - s1 - sd + 40, 5, 5);
+        g.fillRect(c1.x - 5, myShip.y - s1 - sd + 40, 5, 5);
+
+        g.setColor(OTHER_CAR_TWO);
+        g.fillRect(c2.x, myShip.y - s2 - sd, 20, 50);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(c2.x + 20, myShip.y - s2 - sd + 5, 5, 5);
+        g.fillRect(c2.x - 5, myShip.y - s2 - sd + 5, 5, 5);
+        g.fillRect(c2.x + 20, myShip.y - s2 - sd + 40, 5, 5);
+        g.fillRect(c2.x - 5, myShip.y - s2 - sd + 40, 5, 5);
+
+        g.drawString("Car - Dist. travelled", 380, 170);
         String r1 = "Me     " + (-sd);
         g.drawString(r1, 380, 190);
+
         r1 = "C1     " + (s1);
         g.drawString(r1, 380, 210);
+
         r1 = "C2     " + (s2);
         g.drawString(r1, 380, 230);
+
         r1 = "Rank";
         g.drawString(r1, 380, 250);
 
@@ -307,7 +325,6 @@ public class Track extends JFrame implements Runnable {
             // Result not equals OK
             System.exit(0);
         }
-
 
     }
 
